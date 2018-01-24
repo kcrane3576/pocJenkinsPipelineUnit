986ce01ca79da240bab459cd10f23fb1d087744c
crane <kcrane@ippon.fr>
2018-01-23 22:13:09 -0500

    reset gitCheckout to just checkout the code for now

--- src/scm.groovy
+++ src/scm.groovy
@@ -1,14 +1,7 @@
-def checkout(repo){
+def gitCheckout(){
     node{
         stage("checkout"){
-            scm {
-                git {
-                    remote { url(repo) }
-                    branches('master')
-                    scriptPath('Jenkinsfile')
-                    extensions { }  // required as otherwise it may try to tag the repo, which you may not want
-                }
-            }
+            checkout scm
         }
     }
 }
