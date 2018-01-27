953c8d3fab9764e8f9d151d27f9006f996379d0e
crane <kcrane@ippon.fr>
2018-01-26 20:42:51 -0500

    changed pipeline to just run a shell command

--- src/pipeline.groovy
+++ src/pipeline.groovy
@@ -1,30 +1,32 @@
 pipelineJob('Pipeline02') {
 
-    def repo = 'https://bitbucket.org/kcrane/jhipster-basic-micro-mongo.git'
+    sh('ls -a')
 
-    definition {
-        scm.checkout(repo)
-
-        parameters {
-            passwordParameterDefinition {
-                name('SECRET_ACCESS_KEY')
-                defaultValue(null)
-                description('')
-            }
-            gitParameterDefinition {
-                name('branch_selector')
-                type('PT_BRANCH_TAG')
-                defaultValue('origin/master')
-                description('')
-                branch('')
-                branchFilter('.*')
-                tagFilter('*')
-                sortMode('ASCENDING')
-                selectedValue('DEFAULT')
-                useRepository('')
-                quickFilterEnabled(false)
-            }
-        }
-    }
+//    def repo = 'https://bitbucket.org/kcrane/jhipster-basic-micro-mongo.git'
+//
+//    definition {
+//        scm.checkout(repo)
+//
+//        parameters {
+//            passwordParameterDefinition {
+//                name('SECRET_ACCESS_KEY')
+//                defaultValue(null)
+//                description('')
+//            }
+//            gitParameterDefinition {
+//                name('branch_selector')
+//                type('PT_BRANCH_TAG')
+//                defaultValue('origin/master')
+//                description('')
+//                branch('')
+//                branchFilter('.*')
+//                tagFilter('*')
+//                sortMode('ASCENDING')
+//                selectedValue('DEFAULT')
+//                useRepository('')
+//                quickFilterEnabled(false)
+//            }
+//        }
+//    }
 }
 
