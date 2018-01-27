c090b13ad82cae6f624dec7c7b64233817373b21
crane <kcrane@ippon.fr>
2018-01-27 10:33:50 -0500

    added method to start loading json files

---
+++ vars/checkoutSCM.groovy
@@ -0,0 +1,23 @@
+#!/usr/bin/env groovy
+
+void getJenkinsfileConfig() {
+    def serviceConfigDirectory = "../service-config/"
+    def serviceConfigPath = serviceConfigDirectory + "general-config.json"
+
+    def jsonSlurper = new JsonSlurper()
+    def data = jsonSlurper.parseText(new File(serviceConfigPath).text)
+    for(String key : data){
+        println(key)
+    }
+}
+
+def checkout(){
+    node{
+        stage("checkout"){
+            checkout scm
+            sh('ls -a')
+        }
+    }
+
+    getJenkinsfileConfig()
+}
