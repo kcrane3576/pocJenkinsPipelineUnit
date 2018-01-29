f2e1a7a55cb41ec13a41e3428b3b0c2a7daae0fd
crane <kcrane@ippon.fr>
2018-01-29 11:36:27 -0500

    added test methods for finding path to the resources/service-config directory

--- src/main/groovy/PipelineGeneral.groovy
+++ src/main/groovy/PipelineGeneral.groovy
@@ -4,13 +4,30 @@ package main.groovy.mypipeline;
 
 import groovy.json.JsonSlurper
 
-def getConfig(){
+/**
+ * Generates a path to a temporary file location, ending with {@code path} parameter.
+ *
+ * @param path path suffix
+ * @return path to file inside a temp directory
+ */
+@NonCPS
+String createTempLocation(String path) {
+    String tmpDir = pwd tmp: true
+    return tmpDir + File.separator + new File(path).getName()
+}
 
-    def inputFile = new File('../resources/service-config/general.json')
-    def inputJson = new JsonSlurper().parse(inputFile)
-    println(inputJson)
-
-
+/**
+ * Returns the path to a temp location of a script from the global library (resources/ subdirectory)
+ *
+ * @param srcPath path within the resources/ subdirectory of this repo
+ * @param destPath destination path (optional)
+ * @return path to local file
+ */
+String copyGlobalLibraryScript(String srcPath, String destPath = null) {
+    destPath = destPath ?: createTempLocation(srcPath)
+    writeFile file: destPath, text: libraryResource(srcPath)
+    echo "copyGlobalLibraryScript: copied ${srcPath} to ${destPath}"
+    return destPath
 }
 
 def checkout(){
@@ -18,7 +35,7 @@ def checkout(){
         stage("checkout"){
             checkout scm
             sh('ls -a')
-            getConfig()
+            copyGlobalLibraryScript("service-config/general.json")
         }
     }
 }
