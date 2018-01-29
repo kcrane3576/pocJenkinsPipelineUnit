f7a2dfaca630c5eccc8d019123ddfb10b4d3a2ee
crane <kcrane@ippon.fr>
2018-01-29 11:45:07 -0500

    temp moving general.json to try and figure out path issue

--- src/main/groovy/PipelineGeneral.groovy
+++ src/main/groovy/PipelineGeneral.groovy
@@ -35,7 +35,7 @@ def checkout(){
         stage("checkout"){
             checkout scm
             sh('ls -a')
-            copyGlobalLibraryScript("resources/service-config/general.json")
+            copyGlobalLibraryScript("general.json")
         }
     }
 }
