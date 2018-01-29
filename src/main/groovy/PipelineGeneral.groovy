b86545d778afe704278034d2b857ab468b658efe
crane <kcrane@ippon.fr>
2018-01-29 12:28:21 -0500

    added step up from resources repo to try and traverse directory

--- src/main/groovy/PipelineGeneral.groovy
+++ src/main/groovy/PipelineGeneral.groovy
@@ -2,23 +2,14 @@
 
 package main.groovy.mypipeline;
 
-import groovy.json.JsonSlurper
-
-def writeIt(){
-    def functions = libraryResource 'src/main/resources/functions.sh'
-    writeFile file: 'functions.sh', text: functions
-    sh "source function.sh && foofoo"
-}
-
-
 def checkout(){
     node{
         stage("checkout"){
             checkout scm
-//            def libResource = libraryResource "../../src/main/resources/general.json"
-            def libResource = libraryResource "general.json"
+            //TODO this works for top level resources directory
+//            def libResource = libraryResource "general.json"
             println(libResource)
-//            writeIt()
+            def libResource = libraryResource "../src/main/resources/general.json"
             sh('ls -a')
         }
     }
