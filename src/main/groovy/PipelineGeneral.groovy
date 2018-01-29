144030dda2b64590599baa43199e5dd6ac4c30c3
crane <kcrane@ippon.fr>
2018-01-29 12:41:28 -0500

    updated pipeline to load json file from resources directory to mock expected use case with specific service configuration

--- src/main/groovy/PipelineGeneral.groovy
+++ src/main/groovy/PipelineGeneral.groovy
@@ -6,9 +6,7 @@ def checkout(){
     node{
         stage("checkout"){
             checkout scm
-            //TODO this works for top level resources directory
-//            def libResource = libraryResource "general.json"
-            def libResource = libraryResource "../src/main/resources/general.json"
+            def libResource = libraryResource "service-config/general.json"
             println(libResource)
             sh('ls -a')
         }
