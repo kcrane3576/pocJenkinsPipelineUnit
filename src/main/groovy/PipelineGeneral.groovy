2df116bc5fa5ea0b64b6dea73d6ac34913360cb1
crane <kcrane@ippon.fr>
2018-01-29 11:49:32 -0500

    added main/groovy/resources to path

--- src/main/groovy/PipelineGeneral.groovy
+++ src/main/groovy/PipelineGeneral.groovy
@@ -35,7 +35,7 @@ def checkout(){
         stage("checkout"){
             checkout scm
             sh('ls -a')
-            copyGlobalLibraryScript("general.json")
+            copyGlobalLibraryScript("main/groovy/resources/general.json")
         }
     }
 }
