72cec9dc93566a133878e7b4aa1c4fde4806a143
crane <kcrane@ippon.fr>
2018-01-29 12:36:10 -0500

    updated libResource to only traverse 1 level up

--- src/main/groovy/PipelineGeneral.groovy
+++ src/main/groovy/PipelineGeneral.groovy
@@ -8,7 +8,7 @@ def checkout(){
             checkout scm
             //TODO this works for top level resources directory
 //            def libResource = libraryResource "general.json"
-            def libResource = libraryResource "../../src/main/resources/general.json"
+            def libResource = libraryResource "../src/main/resources/general.json"
             println(libResource)
             sh('ls -a')
         }
