d740466bce3990a85882b01dfba774f9db3502f8
crane <kcrane@ippon.fr>
2018-01-29 11:27:12 -0500

    added JsonSlurper to read in file from resources

--- src/main/groovy/PipelineGeneral.groovy
+++ src/main/groovy/PipelineGeneral.groovy
@@ -2,11 +2,23 @@
 
 package main.groovy.mypipeline;
 
+import groovy.json.JsonSlurper
+
+def getConfig(){
+
+    def inputFile = new File('../resources/service-config/general.json')
+    def inputJson = new JsonSlurper().parse(inputFile)
+    println(inputJson)
+
+
+}
+
 def checkout(){
     node{
         stage("checkout"){
             checkout scm
             sh('ls -a')
+            getConfig()
         }
     }
 }
