9395366d209b18637d13a0faab79cc68658af07c
crane <kcrane@ippon.fr>
2018-01-29 12:04:42 -0500

    added writeIt to see where libraryResource is writing files

--- src/main/groovy/PipelineGeneral.groovy
+++ src/main/groovy/PipelineGeneral.groovy
@@ -4,13 +4,22 @@ package main.groovy.mypipeline;
 
 import groovy.json.JsonSlurper
 
+def writeIt(){
+    def functions = libraryResource 'src/main/resources/functions.sh'
+    writeFile file: 'functions.sh', text: functions
+    sh "source function.sh && foofoo"
+}
+
+
 def checkout(){
     node{
         stage("checkout"){
             checkout scm
-            sh('ls -a')
             def libResource = libraryResource "../src/main/resources/general.json"
             println(libResource)
+            writeIt()
+            sh('ls -a')
         }
     }
 }
+
