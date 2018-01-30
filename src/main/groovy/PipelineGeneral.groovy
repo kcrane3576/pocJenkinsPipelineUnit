f099b3b1ce0c3756b45667c40bf6ef4d4b73e7de
crane <kcrane@ippon.fr>
2018-01-29 21:17:12 -0500

    converted string to json

--- src/main/groovy/PipelineGeneral.groovy
+++ src/main/groovy/PipelineGeneral.groovy
@@ -2,12 +2,16 @@
 
 package main.groovy.mypipeline;
 
+import json
+
 def checkout(){
     node{
         stage("checkout"){
             checkout scm
             def libResource = libraryResource "service-config/general.json"
             println(libResource)
+            def data = json.loads(libResource)
+            println(data.name)
             sh('ls -a')
         }
     }
