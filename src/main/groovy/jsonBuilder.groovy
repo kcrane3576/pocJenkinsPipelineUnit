cc096fa782944e6618ccae3533e30bbc0dd72079
crane <kcrane@ippon.fr>
2018-02-10 17:35:12 -0500

    added result of recursive call to combine json to the result map

--- src/main/groovy/jsonBuilder.groovy
+++ src/main/groovy/jsonBuilder.groovy
@@ -70,7 +70,7 @@ def combineJson2(dominant, recessive){
         if(rValue != null){
 
             if(rValue instanceof HashMap){
-                combineJson2(dValue, rValue)
+                result += combineJson2(dValue, rValue)
             }else{
                 result.put(dKey, dValue)
             }
