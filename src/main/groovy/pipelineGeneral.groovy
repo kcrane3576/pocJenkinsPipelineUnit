3d00cddaeee813d70f78de8ab97af3cfc4a2fba6
crane <kcrane@ippon.fr>
2018-02-17 15:34:10 -0500

    updated path to load the file

--- src/main/groovy/pipelineGeneral.groovy
+++ src/main/groovy/pipelineGeneral.groovy
@@ -8,7 +8,7 @@ def checkout(String serviceName){
             checkout scm
 
 //            def configBuilder = main.groovy.configBuilder()
-            def configBuilder = evaluate(new File("configBuilder.groovy"))
+            def configBuilder = evaluate(new File("./configBuilder.groovy"))
             def jenkinsfileConfig = configBuilder.getJenkinsfileConfig(serviceName)
             println(jenkinsfileConfig)
 
