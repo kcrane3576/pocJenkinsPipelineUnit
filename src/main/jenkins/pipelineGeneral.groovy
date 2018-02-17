733eeb5a2ad37ae53571004145cf45ba35d60182
crane <kcrane@ippon.fr>
2018-02-17 16:08:54 -0500

    updated to use load

---
+++ src/main/jenkins/pipelineGeneral.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+package main.groovy
+
+def checkout(String serviceName){
+    node{
+        stage("checkout"){
+            checkout scm
+
+//            def configBuilder = new main.groovy.configBuilder()
+            def configBuilder = loadScript('configBuilder.groovy')
+            def jenkinsfileConfig = configBuilder.getJenkinsfileConfig(serviceName)
+            println(jenkinsfileConfig)
+
+            println(jenkinsfileConfig.obj.id)
+            println(jenkinsfileConfig.name)
+            println(jenkinsfileConfig.env.splunk)
+            println(jenkinsfileConfig.env.vpc)
+
+        }
+    }
+}
