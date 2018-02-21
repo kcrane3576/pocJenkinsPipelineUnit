24f00885b68cc41faadb7435d25f05f94c90fca0
crane <kcrane@ippon.fr>
2018-02-20 20:43:07 -0500

    updated loading of configBuilder

--- src/test/groovy/TestPipelineGeneral.groovy
+++ src/test/groovy/TestPipelineGeneral.groovy
@@ -1,21 +1,48 @@
 import com.lesfurets.jenkins.unit.BasePipelineTest
+import com.lesfurets.jenkins.unit.global.lib.GitSource
+import static com.lesfurets.jenkins.unit.global.lib.LibraryConfiguration.library
 import org.junit.Before
+import org.junit.BeforeClass
+import org.junit.ClassRule
 import org.junit.Test
-
+import org.junit.rules.TemporaryFolder
 
 class TestPipelineGeneral extends BasePipelineTest {
+
+//    @ClassRule
+//    public static TemporaryFolder folder = new TemporaryFolder()
+//
+//    static File temp
+//
+//    @BeforeClass
+//    static void init(){
+//        temp = folder.newFolder("libs")
+//    }
 
     @Override
     @Before
     void setUp() throws Exception {
+        super.setUp()
         helper.baseScriptRoot = 'pocJenkinsPipelineUnit'
         helper.scriptRoots += 'src/main/groovy'
-        super.setUp()
+//        helper.registerSharedLibrary(library("jenkinsfiles")
+//                .defaultVersion('master')
+//                .allowOverride(true)
+//                .targetPath(temp.path)
+//                .retriever(GitSource.gitSource("https://github.com/kcrane3576/pocJenkinsPipelineUnit.git"))
+//                .build())
     }
 
     @Test
     void temp(){
         println("temp")
-        def script = loadScript("pipelineGeneral.groovy")
+//        def script = loadScript("pipelineGeneral.groovy")
+//        script.checkout("ip-service1")
+
+        def script = loadScript("src/main/groovy/configBuilder.groovy")
+        script.getJenkinsfileConfig("ip-service1")
+
+        printCallStack()
+
     }
 }
